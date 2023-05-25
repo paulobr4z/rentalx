f31d3a3ff2e704c18349e26bcd690558316f680c
paulobr4z <paulobrazaraujo@gmail.com>
2023-05-25 11:36:49 -0300

    debugando a aplicação

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,16 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (request, response) => {
-  response.json({ message: "Hello world" })
+  return response.json({ message: "Hello world" })
+})
+
+app.post("/courses", (request, response) => {
+  const { name } = request.body;
+
+  return response.json({ name })
 })
 
 app.listen(3333, () => {
