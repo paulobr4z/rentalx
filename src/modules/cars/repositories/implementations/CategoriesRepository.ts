2ba56d13f41a083cb2a666d3c392ac518ad92330
paulobr4z <paulobrazaraujo@gmail.com>
2023-06-08 01:01:37 -0300

    Separando os repositórios

--- src/modules/cars/repositories/implementations/CategoriesRepository.ts
+++ src/modules/cars/repositories/implementations/CategoriesRepository.ts
@@ -1,9 +1,8 @@
-import { Category } from "../models/Category";
-import {
+import { Category } from "../../models/Category";
+import { 
   ICategoriesRepository,
   ICreateCategoryDTO
-} from "./ICategoriesRepository";
-
+} from "../ICategoriesRepository";
 
 
 class CategoriesRepository implements ICategoriesRepository {
